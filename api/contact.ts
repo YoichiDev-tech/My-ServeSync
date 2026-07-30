2570c9ac669f19c7d20062a956a1243a0d3565c2
YoichiDev-tech <yoichi_dev@proton.me>
2026-07-30 20:13:57 +0100

    Testing and fixing the contact.ts

--- api/contact.ts
+++ api/contact.ts
@@ -4,14 +4,7 @@ const app = express();
 // Needed for JSON body parsing
 app.use(express.json());
 
-app.get("/contact", (req: Request, res: Response) => {
-    return res.status(200).json({
-        success: true,
-        message: "Contact endpoint is alive."
-    });
-});
-// Minimal POST endpoint (Route)
-app.post("/contact", (req: Request, res: Response) => {
+app.post("*", (req: Request, res: Response) => {
     const { name, email, businessType, message } = req.body;
 
     // Basic validation (just initial / will be extended later)
